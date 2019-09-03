cc0add032e40703d84e968ef5b71881417c9b45e
Dragonite <haolin97@gmail.com>
2019-09-03 19:23:46 +0800

    Register default home view and accounts routes

--- whitehat/urls.py
+++ whitehat/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('events/', include('events.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]

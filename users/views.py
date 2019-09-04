21bb19626720f35f330c0894cd5c3a580f55ca8f
Dragonite <haolin97@gmail.com>
2019-09-04 15:12:37 +0800

    Add users index page

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from .models import Users
 
 from django.http import HttpResponse
 
-@staff_member_required
+@staff_member_required()
 def index(request):
-    return HttpResponse("Hello, world. You're at the users index.")
+    users = Users.objects.all()
+    c = {'users': users }
+    return render(request, "user_index.html", c)

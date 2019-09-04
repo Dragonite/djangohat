02b466a285f5bd77741d6259c518c12a5366183d
Dragonite <haolin97@gmail.com>
2019-09-04 10:33:37 +0800

    Fix view for event index and messages for create event

--- events/views.py
+++ events/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from dhooks import Webhook, Embed
 from django.http import HttpResponse
 from events.forms import CreateEventForm
 from .utils import send_embed
+from .models import Event
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.contrib.admin.views.decorators import staff_member_required
 
+
 def index(request):
-    c = {}
+    current_events = Event.objects.filter(time__gte=timezone.now())
+    previous_events = Event.objects.filter(time__lt=timezone.now())
+    c = {'current_events': current_events, 'previous_events': previous_events}
     return render(request, "index.html", c)
 
 
-@login_required
+@staff_member_required
 def create_event(request):
     form = CreateEventForm()
     if request.POST:
@@ -20,6 +26,8 @@ def create_event(request):
             if CreateEventForm(request.POST).is_valid():
                 CreateEventForm(request.POST).save()
                 send_embed(request)
-
+                messages.success(request, 'Event created and deployed to Discord!')
+                return redirect("events:event_index")
+            messages.error(request, 'Invalid Parameters for Event!')
     c = {'form': form }
     return render(request, "create_event.html", c)

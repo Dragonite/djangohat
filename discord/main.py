b5cb0b11c4543b24808911563330958306ad5c5d
Dragonite <haolin97@gmail.com>
2019-11-10 16:36:01 +0800

    Remove comments and model imports

--- discord/main.py
+++ discord/main.py
@@ -10,7 +10,6 @@ sys.path.append(BASE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'whitehat.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whitehat.settings")
 django.setup()
-from users.models import *
 
 prefix = settings.DISCORD_BOT_PREFIX
 
@@ -21,25 +20,6 @@ async def on_ready():
     activity = discord.Activity(name="you hack ethically!", type=discord.ActivityType.watching)
     await bot.change_presence(activity=activity)
     print("Bot connected!")
-
-# @bot.event
-# async def on_message(message):
-#     if message.channel.type == discord.ChannelType.private and message.author != bot.user:
-#         profile = Users.objects.filter(discord_id=message.author.id)
-#         print(profile)
-#         await message.channel.send(message.content)
-#     print(message.channel.type)
-    # if message.channel is None and message.author != bot.user:
-    #     # profile = Users.objects.filter(discord_id=message.author.id)
-    #     # print(profile)
-    #     # await bot.send_message(message.content)
-    #     print('hi')
-
-
-@bot.command()
-async def register(ctx, *args):
-    for arg in args:
-        await ctx.send(arg)
 
 initial_extensions = ['cogs.link']
 

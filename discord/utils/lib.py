2397b8065fe8c348b9d9a90e1cb0b7eed9cf5237
Dragonite <haolin97@gmail.com>
2019-11-10 17:17:12 +0800

    Add club name and current year to lib

--- discord/utils/lib.py
+++ discord/utils/lib.py
@@ -1,11 +1,7 @@
-import discord
+CLUB_NAME = "UWA Ethical Hacking Club"
+CURRENT_YEAR = 2019
 
 COLOR_INFO = 5347285
 COLOR_SUCCESS = 2664261
 COLOR_DANGER = 14431557
 COLOR_WARNING = 16761095
-
-
-def checkBotLoop(message, client):
-    if message.author == client.user:
-        return

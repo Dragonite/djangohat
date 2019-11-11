150c7262744d3bbbc087655b48cd3fe6baabddaf
Dragonite <haolin97@gmail.com>
2019-11-11 22:32:19 +0800

    Register Event model for django admin

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Event Model
 
 class Event(models.Model):
-	
+
 	DEFAULT_TITLE = "Ethical Hacking Meeting - Weekly Meetup"
 	DEFAULT_LOCATION = "G01 at CSSE"
 	DEFAULT_INFO = "Bring your laptops"
@@ -14,3 +14,6 @@ class Event(models.Model):
 	time = models.CharField(max_length=100)
 	info = models.TextField(default=DEFAULT_INFO)
 	date_created = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.time

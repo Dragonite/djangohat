44500305f0beb86d8028dfce58419c13601f4461
Dragonite <haolin97@gmail.com>
2019-11-11 21:40:45 +0800

    Add help comments and fix indenting for event cog

--- discord/cogs/event.py
+++ discord/cogs/event.py
@@ -12,61 +12,63 @@ logger = logging.getLogger()
 
 class EventCog(commands.Cog):
 
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-    def event_help(self):
-        embed = discord.Embed(title='Create an Event',
-                              description='To create an event, please follow the following structure with the `!event` command.\n```!event\n<Title>\n<Location>\n<Time>\n<Information>```',
-                              color=COLOR_INFO, timestamp=datetime.datetime.now())
-        embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=self.bot.user.avatar_url)
-        return embed
+	# Creates the default help embed.
+	def event_help(self):
+		embed = discord.Embed(title='Create an Event',
+							  description='To create an event, please follow the following structure with the `!event` command.\n```!event\n<Title>\n<Location>\n<Time>\n<Information>```',
+							  color=COLOR_INFO, timestamp=datetime.datetime.now())
+		embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=self.bot.user.avatar_url)
+		return embed
 
-    # Gets the next meeting date and time. By default, the time will be the next Wednesday 3PM
-    # inclusive of the current day, i.e. If it is Wednesday, it will return today's date 3PM.
-    def get_next_default_meeting_time(self, d, weekday):
-        days_ahead = (weekday - d.weekday()) % 7
-        if days_ahead < 0: days_ahead += 7
-        next_date_string = (d + datetime.timedelta(days_ahead)).strftime("%A %-d %B at 3PM")
-        return next_date_string
+	# Gets the next meeting date and time. By default, the time will be the next Wednesday 3PM
+	# inclusive of the current day, i.e. If it is Wednesday, it will return today's date 3PM.
+	def get_next_default_meeting_time(self, d, weekday):
+		days_ahead = (weekday - d.weekday()) % 7
+		if days_ahead < 0: days_ahead += 7
+		next_date_string = (d + datetime.timedelta(days_ahead)).strftime("%A %-d %B at 3PM")
+		return next_date_string
 
-    def custom_event(self, arg_list):
-        title = arg_list[0]
-        location = arg_list[1]
-        time = arg_list[2]
-        information = arg_list[3]
-        embed = discord.Embed(title=title, color=COLOR_INFO, timestamp=datetime.datetime.now())
-        embed.add_field(name="Location", value=location, inline=False)
-        embed.add_field(name="Time", value=time, inline=False)
-        embed.add_field(name="Information", value=information, inline=False)
-        embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=self.bot.user.avatar_url)
-        return embed
+	# Creates a custom event based on argument list, created through the event command.
+	def custom_event(self, arg_list):
+		title = arg_list[0]
+		location = arg_list[1]
+		time = arg_list[2]
+		information = arg_list[3]
+		embed = discord.Embed(title=title, color=COLOR_INFO, timestamp=datetime.datetime.now())
+		embed.add_field(name="Location", value=location, inline=False)
+		embed.add_field(name="Time", value=time, inline=False)
+		embed.add_field(name="Information", value=information, inline=False)
+		embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=self.bot.user.avatar_url)
+		return embed
 
-    @commands.command()
-    @has_permissions(administrator=True)
-    async def event(self, ctx, *, arg=None):
-        if arg:
-            arg_list = arg.split('\n')
-            if len(arg_list) == 1 and arg == 'default':
-                arg_list[0]
-                await ctx.send("arg")
-            elif len(arg_list) == 4:
-                channel = self.bot.get_channel(settings.EVENT_CHANNEL)
-                try:
-                    await channel.send(embed=self.custom_event(arg_list))
-                    await(await channel.send("@everyone")).delete()
-                    await ctx.send("successful event creation")
-                except:
-                    await ctx.send("event could not be created")
-            else:
-                await ctx.send(embed=self.event_help())
-        else:
-            await ctx.send(embed=self.event_help())
+	@commands.command()
+	@has_permissions(administrator=True)
+	async def event(self, ctx, *, arg=None):
+		if arg:
+			arg_list = arg.split('\n')
+			if len(arg_list) == 1 and arg == 'default':
+				arg_list[0]
+				await ctx.send("arg")
+			elif len(arg_list) == 4:
+				channel = self.bot.get_channel(settings.EVENT_CHANNEL)
+				try:
+					await channel.send(embed=self.custom_event(arg_list))
+					await(await channel.send("@everyone")).delete()
+					await ctx.send("successful event creation")
+				except:
+					await ctx.send("event could not be created")
+			else:
+				await ctx.send(embed=self.event_help())
+		else:
+			await ctx.send(embed=self.event_help())
 
-        # print(DEFAULT_LOCATION)
-        # print(arg)
-        # print(self.get_next_default_meeting_time(datetime.datetime.now(), WEDNESDAY))
-                        
+		# print(DEFAULT_LOCATION)
+		# print(arg)
+		# print(self.get_next_default_meeting_time(datetime.datetime.now(), WEDNESDAY))
+						
 
 def setup(bot):
-    bot.add_cog(EventCog(bot))
+	bot.add_cog(EventCog(bot))

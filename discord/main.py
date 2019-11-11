6029e853e556f4fbe7c4fb4369ae154614e4c275
Dragonite <haolin97@gmail.com>
2019-11-11 19:16:50 +0800

    Register event cog

--- discord/main.py
+++ discord/main.py
@@ -15,8 +15,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'whitehat.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whitehat.settings")
 django.setup()
 
-prefix = settings.DISCORD_BOT_PREFIX
-
 bot = commands.Bot(command_prefix=settings.DISCORD_BOT_PREFIX)
 
 logging.basicConfig(filename='discord/logs/whitehat-{}.log'.format(datetime.today().strftime('%Y-%m-%d')), filemode='w', format='%(asctime)s - %(message)s',
@@ -31,7 +29,7 @@ async def on_ready():
     print("Bot connected!")
 
 
-initial_extensions = ['cogs.link']
+initial_extensions = ['cogs.link', 'cogs.event']
 
 if __name__ == '__main__':
     for extension in initial_extensions:

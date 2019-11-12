0cdac67c0056165c729fbbc961a1d94d919079fa
Dragonite <haolin97@gmail.com>
2019-11-12 20:40:49 +0800

    Add removed embed for reactions

--- discord/utils/embeds.py
+++ discord/utils/embeds.py
@@ -6,3 +6,8 @@ def successful_role_assigned(bot, role):
 	embed = discord.Embed(title="Added to {}!".format(role), color=COLOR_SUCCESS, timestamp=datetime.now())
 	embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=bot.user.avatar_url)
 	return embed
+
+def successful_role_removed(bot, role):
+	embed = discord.Embed(title="Removed from {}!".format(role), color=COLOR_SUCCESS, timestamp=datetime.now())
+	embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=bot.user.avatar_url)
+	return embed

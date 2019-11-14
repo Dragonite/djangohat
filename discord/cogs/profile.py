b00fba8c6f8301440576a313d2bdd843aadabd6c
Dragonite <haolin97@gmail.com>
2019-11-14 17:26:19 +0800

    Add @ing someone as a profile search feature

--- discord/cogs/profile.py
+++ discord/cogs/profile.py
@@ -28,14 +28,44 @@ class ProfileCog(commands.Cog):
         embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=self.bot.user.avatar_url)
         return embed
 
+    def other_missing_profile(self, ctx):
+        embed = discord.Embed(title="No profile exists.", color=COLOR_INFO, timestamp=datetime.datetime.now())
+        embed.set_footer(text="{} {}".format(CLUB_NAME, CURRENT_YEAR), icon_url=self.bot.user.avatar_url)
+        return embed
+
     @commands.command()
     async def profile(self, ctx, *args):
         if args:
+            if len(args) == 1:
+                # Handles nicknames that generate an additional !
+                if args[0].startswith("<@!") and args[0].endswith(">"):
+                    arg_first = args[0].replace("!", "")
+                else:
+                    arg_first = args[0]
+                # Handles an @ as a search
+                if arg_first.startswith("<@") and arg_first.endswith(">"):
+                    query = arg_first[2:-1]
+                    try:
+                        query = int(query)
+                        profile = Users.objects.filter(discord_id=query).first()
+                        print(profile)
+                        if profile:
+                            await ctx.send(embed=self.existing_profile(ctx, profile))
+                        else:
+                            await ctx.send(embed=self.other_missing_profile(ctx))
+                    except Exception:
+                        await ctx.send(embed=self.other_missing_profile(ctx))
+                # Search by tag contents
+                else:
+                    query = args[0]
+                    Users.objects.filter(discord_tag__contains=query)
+
+                # Searches by tag
+
+                print(query)
             # Handle searches, should regex for an @, their tag and their discord ID.
             for arg in args:
                 await ctx.send(arg)
-                print(arg)
-            print("args")
         else:
             try:
                 profile = Users.objects.get(discord_id=ctx.message.author.id)

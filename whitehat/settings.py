05fe5ac5a8bd394d273a071929989116c2dd2d8d
Dragonite <haolin97@gmail.com>
2019-11-15 12:54:02 +0800

    Make link conditional in modal profile

--- whitehat/settings.py
+++ whitehat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .settings_prod import *
+from .settings_dev import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
